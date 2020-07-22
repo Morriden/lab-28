716846695413ae7885285b7afa75068d05efb32c
Erik Ford <ekford18@gmail.com>
2020-07-22 13:55:18 -0700

    added service routes

--- src/Services/ActionRoutes.js
+++ src/Services/ActionRoutes.js
@@ -3,14 +3,26 @@ export const GetRoute = (userInput) => {
     .then(res => res.json())
 }
 
-export const PostRoute = () => {
-
+export const PostRoute = (link, userInput) => {
+    return fetch(`${link}`, {
+        method: 'POST',
+        body: `${userInput}`
+    })
+    .then(res => res.json())
 }
 
-export const PutRoute = () => {
-
+export const PutRoute = (link, userInput) => {
+    return fetch(`${link}`, {
+        method: 'PUT',
+        body: `${userInput}`
+    })
+    .then(res => res.json())
 }
 
-export const DeleteRoute = () => {
-    
+export const DeleteRoute = (link, userInput) => {
+    return fetch(`${link}`, {
+        method: 'DELETE',
+        body: `${userInput}`
+    })
+    .then(res => res.json())
 }

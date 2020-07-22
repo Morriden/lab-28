d876d152e15eb04fa2dc8a20d338f0e21a875aaf
Erik Ford <ekford18@gmail.com>
2020-07-22 11:43:04 -0700

    written test

---
+++ src/components/Form/Form.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Form from './Form';
+
+describe('Form component', () => {
+  it('should render to input fields and one button for now.', () => {
+    const wrapper = shallow(
+    <Form 
+        linkInput="somewebsite.com"
+        bodyInput="someJSONbodyinfo"
+        onLinkChange={() => {}}
+        onBodyChange={() => {}}
+        onSubmit={() => {}}
+    />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
